638f18aed8f7f7b03357eb4be12799ae8cb3a02c
Yeray Darias <ydarias@gmail.com>
2013-12-08 10:29:58 +0000

    Corregido el comportamiento del formulario y añadido el listado inicial con la búsqueda de juegos sin filtro mediante inyección de dependencias de Spring.

--- otogami-server/src/main/java/com/otogami/server/servlets/VideogameServlet.java
+++ otogami-server/src/main/java/com/otogami/server/servlets/VideogameServlet.java
@@ -1,43 +1,54 @@
 package com.otogami.server.servlets;
 
+import com.otogami.server.facade.VideogameFacade;
+import com.otogami.server.facade.VideogameSearchSpecification;
+import com.otogami.server.model.VideogameEntity;
 import freemarker.template.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class VideogameServlet extends HttpServlet {
 
+    @Autowired private VideogameFacade videogameFacade;
+
+    private Configuration freemakerConfiguration;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
+
+        freemakerConfiguration = new Configuration();
+        freemakerConfiguration.setServletContextForTemplateLoading(getServletContext(), "WEB-INF/templates");
+        freemakerConfiguration.setObjectWrapper(new DefaultObjectWrapper());
+        freemakerConfiguration.setDefaultEncoding("UTF-8");
+        freemakerConfiguration.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        freemakerConfiguration.setIncompatibleImprovements(new Version(2, 3, 20));
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setHeader("Content-Type", "text/html");
-
-        Configuration configuration = new Configuration();
-        configuration.setServletContextForTemplateLoading(getServletContext(), "WEB-INF/templates");
-        configuration.setObjectWrapper(new DefaultObjectWrapper());
-        configuration.setDefaultEncoding("UTF-8");
-        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
-        configuration.setIncompatibleImprovements(new Version(2, 3, 20));
+        VideogameSearchSpecification searchSpecification = new VideogameSearchSpecification();
+        List<VideogameEntity> videogames = videogameFacade.getVideogames(searchSpecification);
 
         Map<String, Object> data = new HashMap<String, Object>();
+        data.put("videogames", videogames);
 
-        Template template = configuration.getTemplate("videogames.ftl");
+        Template template = freemakerConfiguration.getTemplate("videogames.ftl");
 
+        response.setHeader("Content-Type", "text/html");
         PrintWriter out = response.getWriter();
         try {
             template.process(data, out);
@@ -45,6 +56,11 @@ public class VideogameServlet extends HttpServlet {
             out.println("ERROR");
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
     }
 
 }
--- otogami-server/src/main/webapp/WEB-INF/templates/videogames.ftl
+++ otogami-server/src/main/webapp/WEB-INF/templates/videogames.ftl
@@ -2,13 +2,66 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Hello World</title>
+    <title>Videogames finder</title>
 </head>
 <body>
-<h1>Hello World</h1>
 
-<p>
-    Jamie was here.
-</p>
+<h1>Videogames finder</h1>
+
+<form role="form" name="search-form" action="search" method="get">
+    <div class="form-group">
+        <label for="videogame-title">Title</label>
+        <input type="text" class="form-control" name="title" id="videogame-title" placeholder="Title here ...">
+    </div>
+    <div class="form-group">
+        <label for="videogame-platform">Platform</label>
+        <select name="platform" id="videogame-platform" class="form-control">
+            <option value="ps3">Play Station 3</option>
+            <option value="ps4">Play Station 4</option>
+            <option value="xbox360">xBox 360</option>
+            <option value="xboxone">xBox One</option>
+        </select>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input name="availability" id="videogame-availability" type="checkbox"> Only available
+        </label>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input name="price" id="videogame-price" type="checkbox"> Cheapers first
+        </label>
+    </div>
+
+    <button id="submit-button" type="submit" class="btn btn-default">Submit</button>
+</form>
+
+<#if videogames??>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Title</th>
+                <th>Platform</th>
+                <th>Available</th>
+                <th>Price</th>
+                <th>Link</th>
+			</tr>
+		</thead>
+		<tbody>
+			<#list videogames as videogame>
+			    <tr>
+					<td>${videogame.title}</td>
+					<td>${videogame.platform}</td>
+                    <td>${videogame.availability}</td>
+                    <td>${videogame.price} &#8364;</td>
+                    <td>${videogame.url}</td>
+			    </tr>
+			</#list>
+		</tbody>
+	</table>
+<#else>
+	Please, introduce some filters and press submit button.
+</#if>
+
 </body>
 </html>
